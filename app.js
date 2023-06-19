badacd7aa5f0b3e867d033e0f7b7e27920a96705
manideepbangaru <bmd994@gmail.com>
2023-06-19 20:08:33 +0530

    finished navbar

---
+++ app.js
@@ -0,0 +1,23 @@
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+    // console.log(links.classList);
+    // console.log(links.classList.contains('links'));
+
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links')
+    // }
+    // else{
+    //     links.classList.add("show-links")
+    // }
+
+    // one liner
+    links.classList.toggle("show-links");
+})
+
+// window.addEventListener('mouseup', function(e){
+//     if(e.target != links){
+//         links.classList.remove("show-links")
+//     }
+// })
