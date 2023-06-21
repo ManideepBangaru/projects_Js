56dd5bd3f59c7016b51eb8e536b89561e8bd61bb
manideepbangaru <bmd994@gmail.com>
2023-06-21 19:38:55 +0530

    testing simple iterator

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
 
 btn.addEventListener("click", () => {
-    
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.background = colors[randomNumber]
+    color.textContent = colors[randomNumber]
 })
+
+getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
